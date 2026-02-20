e01fd917f64feca3e5243e4da59fe83085e5e7a4
MoulikB <moulikbhatia@icloud.com>
2026-02-20 17:48:45 -0600

    3 Vaccines down, pushing code to production on a friday without testing!!!!

--- VaccineEligibility.py
+++ VaccineEligibility.py
@@ -1,17 +1,77 @@
 from py_rules.components import Condition, Result, Rule
 from py_rules.engine import RuleEngine
+from datetime import date
 
-def Hib(Patient):
-    condition = Condition('age', '>', 6) & Condition('age', '<', 59)
+# def Hib(Patient):
+#     condition = Condition('age', '>', 6) & Condition('age', '<', 59)
+#     # Create a condition
+
+#     doNotAdminister = ["Asplenia" , "hyposplenism" , "Cancer" , "HIV"  , "Hemoglobinopathies" , "HIV" ]
+
+
+#     # Create a result
+#     result = Result('message', 'str', 'Eligible for vaccine!')
+
+#     rule = Rule('Hib').If(condition).Then(result)
+
+#     # initialise a new instance of RuleEngine with context
+#     context = {'age': Patient["age"]}
+
+#     engine = RuleEngine(context)
+
+#     return (engine.evaluate(rule))
+#     # Eligible for vaccine!
+
+# def PolioEligibility(Patient):
+#     condition = Condition('age', '>', 48) & Condition('age', '<', 84)
+#     # Create a condition
+
+
+#     # Create a result
+#     result = Result('message', 'str', 'Eligible for vaccine!')
+
+#     rule = Rule('Polio').If(condition).Then(result)
+
+#     # initialise a new instance of RuleEngine with context
+#     context = {'age': Patient["age"]}
+
+#     engine = RuleEngine(context)
+
+#     return (engine.evaluate(rule))
+#     # Eligible for vaccine!
+
+# def Rotavirus(Patient):
+#     FirstDoseCondition = Condition('age', '>', 1.3) & Condition('age', '<', 3.5)
+
+#     FirstDoseDate = Patient["Medical History"]["Rotavirus First Dose"]
+
+#     today = date.today()
+
+#     firstDoseCompletedCondition = Condition('age', '>', 1.3)
+#     SecondDoseCondition = Condition()
+#     # Create a condition
+
+#     # Create a result
+#     result = Result('message', 'str', 'Eligible for vaccine!')
+
+#     rule = Rule('Polio').If(condition).Then(result)
+
+#     # initialise a new instance of RuleEngine with context
+#     context = {'age': Patient["age"]}
+
+#     engine = RuleEngine(context)
+
+#     return (engine.evaluate(rule))
+#     # Eligibility for vaccine!
+
+def MMRV(Patient):
+    condition =  Condition('age', '<', 144)
     # Create a condition
 
-    doNotAdminister = ["Asplenia" , "hyposplenism" , "Cancer" , "HIV"  , "Hemoglobinopathies" , "HIV" ]
-
-
     # Create a result
-    result = Result('message', 'str', 'Eligible for vaccine!')
+    result = Result('message', 'str', 'Eligible for MMRV vaccine!')
 
-    rule = Rule('Hib').If(condition).Then(result)
+    rule = Rule('MMRV').If(condition).Then(result)
 
     # initialise a new instance of RuleEngine with context
     context = {'age': Patient["age"]}
@@ -21,37 +81,19 @@ def Hib(Patient):
     return (engine.evaluate(rule))
     # Eligible for vaccine!
 
-def PolioEligibility(Patient):
-    condition = Condition('age', '>', 48) & Condition('age', '<', 84)
-    # Create a condition
+def IPV(Patient) :
+    condition =  Condition('age', '<', 216)
 
-    doNotAdminister = ["Asplenia" , "hyposplenism" , "Cancer" , "HIV"  , "Hemoglobinopathies" , "HIV" ]
+    conditions = Patient["Chronic Condtions"]
 
+    search_string = "unimmunised"
 
-    # Create a result
-    result = Result('message', 'str', 'Eligible for vaccine!')
+    if not (("Unimmunized" in conditions) or ("Incompletely immunized" in conditions) ): 
+        return False
+    
+    result = Result('message', 'str', 'Eligible for IPV vaccine!')
 
-    rule = Rule('Polio').If(condition).Then(result)
-
-    # initialise a new instance of RuleEngine with context
-    context = {'age': Patient["age"]}
-
-    engine = RuleEngine(context)
-
-    return (engine.evaluate(rule))
-    # Eligible for vaccine!
-
-def PolioEligibility(Patient):
-    condition = Condition('age', '>', 48) & Condition('age', '<', 84)
-    # Create a condition
-
-    doNotAdminister = ["Asplenia" , "hyposplenism" , "Cancer" , "HIV"  , "Hemoglobinopathies" , "HIV" ]
-
-
-    # Create a result
-    result = Result('message', 'str', 'Eligible for vaccine!')
-
-    rule = Rule('Polio').If(condition).Then(result)
+    rule = Rule('IPV').If(condition).Then(result)
 
     # initialise a new instance of RuleEngine with context
     context = {'age': Patient["age"]}
@@ -59,25 +101,29 @@ def PolioEligibility(Patient):
     engine = RuleEngine(context)
 
     return (engine.evaluate(rule))
-    # Eligible for vaccine!
 
-def Rotavirus(Patient):
-    FirstDoseCondition = Condition('age', '>', 1.3) & Condition('age', '<', 3.5)
-    SecondDoseCondition = Condition()
-    # Create a condition
+def Varicella(Patient):
+    
+    patientConditions = Patient["Chronic Condtions"]
 
-    doNotAdminister = ["Asplenia" , "hyposplenism" , "Cancer" , "HIV"  , "Hemoglobinopathies" , "HIV" ]
+    searchStrings = ["chronic salicylate therapy" , "HIV"  , "Dialysis" , "Cystic fibrosis" , "Organ transplant" , "Cancer"]
 
+    diseaseCondition = False
 
-    # Create a result
-    result = Result('message', 'str', 'Eligible for vaccine!')
+    for i in patientConditions:
+        if (i in searchStrings):
+            diseaseCondition = True
+    
+    condition =  Condition('age', '<', 192) | Condition('diseaseCondition' , '==' , 1)
+    
+    result = Result('message', 'str', 'Eligible for Varicella vaccine!')
 
-    rule = Rule('Polio').If(condition).Then(result)
+    rule = Rule('Varicella').If(condition).Then(result)
 
     # initialise a new instance of RuleEngine with context
-    context = {'age': Patient["age"]}
+    context = {'age': Patient["age"] , 'diseaseCondition' : diseaseCondition}
 
     engine = RuleEngine(context)
 
     return (engine.evaluate(rule))
-    # Eligible for vaccine!
+
