1dca81013634a6ac05501ef1459e10c7a2613331
MoulikB <moulikbhatia@icloud.com>
2026-02-21 00:55:24 -0600

    removed unnecesary imports

--- src/model/RuleEngine.js
+++ src/model/RuleEngine.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-import { VaccineRule } from "./VaccineRule.js";
-import { Patient } from "./Patient.js";
-
->>>>>>> 1e645d6c0be5291181cece46bd365b45ac9834eb
 export class RuleEngine {
   #rules;
 
