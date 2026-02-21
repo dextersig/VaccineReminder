1acdfa86d438439d9fed835c78fb8e001b6d6a71
UMssquared <Ums2@umanitoba.ca>
2026-02-21 03:41:31 -0600

    MMRv started

--- src/model/VaccineEligibility.js
+++ src/model/VaccineEligibility.js
@@ -4,8 +4,13 @@ import { VaccineStatus } from "./VaccineStatus.js";
 export class MMRV extends VaccineRule {
   name = "MMRV";
   checkEligibility(patient) {
-    if (patient.ageInYears < 12) return VaccineStatus.ELIGIBLE_NOW;
-    return VaccineStatus.NOT_ELIGIBLE;
+    if ((patient.ageInYears <= 12)) { //NEED TO CALL V AND MMR
+      return VaccineStatus.ELIGIBLE_NOW;
+    }else if(patient.vaccinesAndDates.includes("MMRV Dose 1") && patient.vaccinesAndDates.includes("MMRV Dose 2")){
+      return VaccineStatus.COMPLETED;
+    }else{
+      return VaccineStatus.NOT_ELIGIBLE;
+    }
   }
 }
 
@@ -40,9 +45,11 @@ export class Varicella extends VaccineRule {
   name = "Varicella";
 
   checkEligibility(patient) {
-    if (
-      (((patient.birthYear >= 2008) && (patient.ageInYears >= 1)) ||
-      containsAny(this.search_strings, patient.conditions)) && (!(patient.vaccinesAndDates.includes("Varicella Dose 1") && patient.vaccinesAndDates.includes("Varicella Dose 2")))){
+    if ((((patient.birthYear >= 2008) && (patient.ageInYears >= 1)) ||
+      containsAny(this.search_strings, patient.conditions)) && 
+      ((!(patient.vaccinesAndDates.includes("Varicella Dose 1") && patient.vaccinesAndDates.includes("Varicella Dose 2"))) &&
+       ((!(patient.vaccinesAndDates.includes("Varicella Dose 1") && patient.vaccinesAndDates.includes("MMRV Dose 1"))) &&
+      (!(patient.vaccinesAndDates.includes("MMRV Dose 1") && patient.vaccinesAndDates.includes("MMRV Dose 2")))))){
         return VaccineStatus.ELIGIBLE_NOW;
       }else if(patient.vaccinesAndDates.includes("Varicella Dose 1") && patient.vaccinesAndDates.includes("Varicella Dose 2")){
         return VaccineStatus.COMPLETED;
