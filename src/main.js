b27f632fb5c5c59429556c5c6a7acbff2e47d5ff
MoulikB <moulikbhatia@icloud.com>
2026-02-21 12:38:56 -0600

    Curr working

--- src/main.js
+++ src/main.js
@@ -5,125 +5,156 @@ import { IPV, MMR, MMRV, Varicella } from "./model/VaccineEligibility.js";
 import emailjs from "@emailjs/browser";
 
 emailjs.init({
-publicKey: "-X43-bLZm01-sjqVv",
+  publicKey: "-X43-bLZm01-sjqVv",
 });
 
 const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", () => {
-const firstName = document.getElementById("firstName").value;
-const lastName = document.getElementById("lastName").value;
-const birthDay = document.getElementById("birthDay").value;
-let conditions = [];
-let vaccinesAndDates = [];
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const birthDay = document.getElementById("birthDay").value;
+  let vaccinesAndDates = [];
 
-//risk factors
-if (document.getElementById("Chronic salicylate therapy").checked) {
-conditions.push(document.getElementById("Chronic salicylate therapy").value);
-}
-if (document.getElementById("HIV").checked) {
-conditions.push(document.getElementById("HIV").value);
-}
-if (document.getElementById("Asplenia").checked) {
-conditions.push(document.getElementById("Asplenia").value);
-}
-if (document.getElementById("Dialysis/Renal Disease").checked) {
-conditions.push(document.getElementById("Dialysis/Renal Disease").value);
-}
-if (document.getElementById("Cystic fibrosis").checked) {
-conditions.push(document.getElementById("Cystic fibrosis").value);
-}
-if (document.getElementById("Organ transplant").checked) {
-conditions.push(document.getElementById("Organ transplant").value);
-}
-if (document.getElementById("Cancer").checked) {
-conditions.push(document.getElementById("Cancer").value);
-}
-if (document.getElementById("Hematopoietic stem cell transplant").checked) {
-conditions.push(document.getElementById("Hematopoietic stem cell transplant").value);
-}
-if (document.getElementById("CAR T-cell therapy recipients").checked) {
-conditions.push(document.getElementById("CAR T-cell therapy recipients").value);
-}
-if (document.getElementById("Under the care of a haematologist or oncologist").checked) {
-conditions.push(document.getElementById("Under the care of a haematologist or oncologist").value);
-}
-if (document.getElementById("Malignant neoplasms").checked) {
-conditions.push(document.getElementById("Malignant neoplasms").value);
-}
-if (document.getElementById("Hyposplenic").checked) {
-conditions.push(document.getElementById("Hyposplenic").value);
-}
-if (document.getElementById("Incompletely Immunized - OPV and less than 2 IPV Doses").checked) {
-conditions.push(document.getElementById("Incompletely Immunized - OPV and less than 2 IPV Doses").value);
-}
+  //risk factors
+  if (document.getElementById("Chronic salicylate therapy").checked) {
+    conditions.push(
+      document.getElementById("Chronic salicylate therapy").value,
+    );
+  }
+  if (document.getElementById("HIV").checked) {
+    conditions.push(document.getElementById("HIV").value);
+  }
+  if (document.getElementById("Asplenia").checked) {
+    conditions.push(document.getElementById("Asplenia").value);
+  }
+  if (document.getElementById("Dialysis/Renal Disease").checked) {
+    conditions.push(document.getElementById("Dialysis/Renal Disease").value);
+  }
+  if (document.getElementById("Cystic fibrosis").checked) {
+    conditions.push(document.getElementById("Cystic fibrosis").value);
+  }
+  if (document.getElementById("Organ transplant").checked) {
+    conditions.push(document.getElementById("Organ transplant").value);
+  }
+  if (document.getElementById("Cancer").checked) {
+    conditions.push(document.getElementById("Cancer").value);
+  }
+  if (document.getElementById("Hematopoietic stem cell transplant").checked) {
+    conditions.push(
+      document.getElementById("Hematopoietic stem cell transplant").value,
+    );
+  }
+  if (document.getElementById("CAR T-cell therapy recipients").checked) {
+    conditions.push(
+      document.getElementById("CAR T-cell therapy recipients").value,
+    );
+  }
+  if (
+    document.getElementById("Under the care of a haematologist or oncologist")
+      .checked
+  ) {
+    conditions.push(
+      document.getElementById("Under the care of a haematologist or oncologist")
+        .value,
+    );
+  }
+  if (document.getElementById("Malignant neoplasms").checked) {
+    conditions.push(document.getElementById("Malignant neoplasms").value);
+  }
+  if (document.getElementById("Hyposplenic").checked) {
+    conditions.push(document.getElementById("Hyposplenic").value);
+  }
+  if (
+    document.getElementById(
+      "Incompletely Immunized - OPV and less than 2 IPV Doses",
+    ).checked
+  ) {
+    conditions.push(
+      document.getElementById(
+        "Incompletely Immunized - OPV and less than 2 IPV Doses",
+      ).value,
+    );
+  }
 
-//vaccines
-if (document.getElementById("MMRV Dose 1").checked) {
-vaccinesAndDates.push(document.getElementById("MMRV Dose 1").name);
-vaccinesAndDates.push(document.getElementById("dayMMRVDose1").value);
-}
-if (document.getElementById("MMRV Dose 2").checked) {
-vaccinesAndDates.push(document.getElementById("MMRV Dose 2").name);
-vaccinesAndDates.push(document.getElementById("dayMMRVDose2").value);
-}
-if (document.getElementById("IPV").checked) {
-vaccinesAndDates.push(document.getElementById("IPV").name);
-vaccinesAndDates.push(document.getElementById("dayIPV").value);
-}
-if (document.getElementById("Varicella Dose 1").checked) {
-vaccinesAndDates.push(document.getElementById("Varicella Dose 1").name);
-vaccinesAndDates.push(document.getElementById("dayVaricellaDose1").value);
-}
-if (document.getElementById("Varicella Dose 2").checked) {
-vaccinesAndDates.push(document.getElementById("Varicella Dose 2").name);
-vaccinesAndDates.push(document.getElementById("dayVaricellaDose2").value);
-}
+  //vaccines
+  if (document.getElementById("MMRVDose1").checked) {
+    vaccinesAndDates.push(document.getElementById("MMRV Dose 1").name);
+    vaccinesAndDates.push(document.getElementById("dayMMRVDose1").value);
+  }
+  if (document.getElementById("MMRVDose2").checked) {
+    vaccinesAndDates.push(document.getElementById("MMRV Dose 2").name);
+    vaccinesAndDates.push(document.getElementById("dayMMRVDose2").value);
+  }
+  if (document.getElementById("IPVDose1").checked) {
+    vaccinesAndDates.push(document.getElementById("IPV Dose 1").name);
+    vaccinesAndDates.push(document.getElementById("dayIPVDose1").value);
+  }
 
+  if (document.getElementById("IPVDose2").checked) {
+    vaccinesAndDates.push(document.getElementById("IPV Dose 2").name);
+    vaccinesAndDates.push(document.getElementById("dayIPVDose2").value);
+  }
 
-const name = `${firstName} ${lastName}`;
+  if (document.getElementById("MMRDose1").checked) {
+    vaccinesAndDates.push(document.getElementById("MMR Dose 1").name);
+    vaccinesAndDates.push(document.getElementById("dayMMRDose1").value);
+  }
 
-if (!name || !birthDay) {
-alert("Missing input");
-return;
-}
+  if (document.getElementById("MMRDose2").checked) {
+    vaccinesAndDates.push(document.getElementById("MMR Dose 2").name);
+    vaccinesAndDates.push(document.getElementById("dayMMRDose2").value);
+  }
 
-const dob = new Date(birthDay);
+  if (document.getElementById("VaricellaDose1").checked) {
+    vaccinesAndDates.push(document.getElementById("Varicella Dose 1").name);
+    vaccinesAndDates.push(document.getElementById("dayVaricellaDose1").value);
+  }
+  if (document.getElementById("VaricellaDose2").checked) {
+    vaccinesAndDates.push(document.getElementById("Varicella Dose 2").name);
+    vaccinesAndDates.push(document.getElementById("dayVaricellaDose2").value);
+  }
 
-const patient = new Patient(name, dob, conditions, vaccinesAndDates);
+  const name = `${firstName} ${lastName}`;
 
-const engine = new RuleEngine([new Varicella(), new MMRV(), new IPV()]);
+  if (!name || !birthDay) {
+    alert("Missing input");
+    return;
+  }
 
-const results = engine.evaluate(patient);
+  const dob = new Date(birthDay);
 
-const userEmail = document.getElementById("email").value;
-// format eligibility results nicely
-const formattedResults = results
-.map((r) => `${r.vaccine}: ${r.status}`)
-.join("\n");
+  const patient = new Patient(name, dob, conditions, vaccinesAndDates);
 
-const templateParams = {
-patient_name: name,
-birthday: birthDay,
-conditions: conditions.join(", "),
-results: formattedResults,
-user_email: userEmail,
-};
+  const engine = new RuleEngine([new Varicella(), new MMRV(), new IPV()]);
 
-emailjs
+  const results = engine.evaluate(patient);
+
+  const userEmail = document.getElementById("email").value;
+  // format eligibility results nicely
+  const formattedResults = results
+    .map((r) => `${r.vaccine}: ${r.status}`)
+    .join("\n");
+
+  const templateParams = {
+    patient_name: name,
+    birthday: birthDay,
+    conditions: conditions.join(", "),
+    results: formattedResults,
+    user_email: userEmail,
+  };
+
+  emailjs
     .send("service_cphd9lc", "template_hn5qc5r", templateParams)
-.then(() => {
-console.log("Email sent successfully");
-})
-.catch((error) => {
-console.error("Email failed:", error);
-});
+    .then(() => {
+      console.log("Email sent successfully");
+    })
+    .catch((error) => {
+      console.error("Email failed:", error);
+    });
 
-
-
-
-document.querySelector("#app").innerHTML = `
+  document.querySelector("#app").innerHTML = `
    <h1>Eligibility Results</h1>
    ${results.map((r) => `<p>${r.vaccine}: ${r.status}</p>`).join("")}
  `;
-})
+});
